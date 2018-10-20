e1bb709e0b6229945036cb56a585757c885325b2
IanRay10 <imray@oakland.edu>
2018-10-20 14:13:45 -0400

    got profeesor names and search page

--- main.js
+++ main.js
@@ -33,3 +33,95 @@ function listener() {
         runScript();
     }
 }
+function runScript() {
+
+    professors.exits = function(name) {
+        return this.hasOwnProperty(name);
+    };
+
+    var schoolName = encodeURI("Oakland university");
+
+    var professorIndex = 0;
+    var currentProfessorNames;
+
+    while (professorName !== "undefined") {
+
+        currentProfessorNames = getProfessorNames(professorIndex);
+
+        if (isValidName(professorName)) {
+            triesCount = 0;
+            getProfessorSearchPage(professorIndex, currentProfessorNames, schoolName);
+        } else {
+            //TODO: Future Implementation.
+            professors.professorName = {};
+            professors.professorName.professorRating = "N/A";
+        }
+
+        professorIndex++;
+    }
+}
+function isValidName(name) {
+
+    return (professorName !== "Staff" &&
+        professorName !== "undefined" &&
+        professorName !== "TBA .");
+}
+function getProfessorNames(indexOfProfessor) {
+    try {
+        names = document.getElementById("ptifrmtgtframe").contentWindow.document.getElementById(professorMethodClass + indexOfProfessor).innerHTML;
+
+        names = names.split(",");
+
+        // Add last names to the list.
+        var maxNames = names.length;
+        for (var i = 0; i < maxNames; i++) {
+            var lastName = getLastName(names[i]);
+            names.push(lastName);
+        }
+        return names;
+
+    } catch (err) {
+        professorName = "undefined";
+    }
+}
+function getProfessorSearchPage(professorIndex, currentProfessorNames, schoolName) {
+
+    var message = {
+        method: "POST",
+        action: "xhttp",
+        url: "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=" + schoolName + "&queryoption=HEADER&query=" + currentProfessorNames[0] + "&facetSearch=true",
+        data: "",
+        link: searchPageURL,
+        index: professorIndex,
+        professorNames: currentProfessorNames
+    };
+
+    chrome.runtime.sendMessage(message, getProfessorSearchPageCallback);
+}
+function getProfessorSearchPageCallback(response) {
+
+    var responseText = response.response;
+
+    var resultsTest = responseText.indexOf("Your search didn't return any results.");
+
+
+    if (foundResult(responseText)) {
+
+        var htmlDoc = getDOMFromString(responseText);
+
+        var professorClass = htmlDoc.getElementsByClassName("listing PROFESSOR")[0].getElementsByTagName("a")[0];
+        searchPageURL = "http://www.ratemyprofessors.com" + professorClass.getAttribute("href");
+
+        getProfessorRating(response.professorIndex, searchPageURL);
+
+    } else if(triesCount < response.professorNames.length) {
+
+        getProfessorSearchPage(response.professorIndex, response.professorNames[triesCount], encodeURI("Oakland university"));
+        triesCount++;
+    }
+}
+
+
+
+
+
