6e65ff1dcb9f5caa19120ede1f19616b3318317b
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2018-10-20 12:11:57 -0400

    added introductory comments

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+/**
+* 
+* RateMyOakland - A Google Chrome axtension
+*
+* Created by Zainab Siddiqui, Ian Ray, Audrey Nguyen, Ariana Lee, and Karen Feun
+*
+* This file is where all the magic happens.
+*
+**/
