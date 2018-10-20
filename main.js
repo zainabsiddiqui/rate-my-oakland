b0baa76dc9a24309377733973476f5b6204d48dd
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2018-10-20 13:44:23 -0400

    declared variables, added event listener

--- main.js
+++ main.js
@@ -7,3 +7,29 @@
 * This file is where all the magic happens.
 *
 **/
+var professorName = "";
+var searchPageURL = "";
+var professorRating = "";
+
+var professors = {};
+
+var triesCount;
+
+var professorMethodClass = "email";
+
+
+var timeout = null;
+document.addEventListener("DOMSubtreeModified",
+	function() {
+		if(timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeOut(listener, 1000);
+	}, false); 
+
+function listener() {
+    resetValues();
+    if (getUserMethod()) {
+        runScript();
+    }
+}
